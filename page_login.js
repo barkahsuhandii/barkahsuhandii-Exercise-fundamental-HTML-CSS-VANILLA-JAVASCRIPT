1795baa1be0891916c3527f27f93c18adefa54af
barkahsuhandii <113780142+barkahsuhandii@users.noreply.github.com>
2022-11-13 19:57:43 +0700

    Set Time Out

--- page_login.js
+++ page_login.js
@@ -1,11 +1,22 @@
 
-
 document.getElementById("Proses").addEventListener('click',function(e){
 
     let UserName = document.getElementById("InputUserName").value
     let Password = document.getElementById("InputPassword").value
-    
-    document.getElementById("TampilUserName").innerHTML=UserName;
-    document.getElementById("TampilPassword").innerHTML=Password;
+  
 
+    if (UserName =='' && Password == ''){
+      document.getElementById("TampilUserName").innerHTML="Data Tidak Ada";
+      document.getElementById("TampilPassword").innerHTML=null;
+    }else
+    {
+
+    document.getElementById("TampilUserName").innerHTML="User Name Anda"+"  "+UserName;
+   document.getElementById("TampilPassword").innerHTML="Password Anda"+"  "+Password;
+   document.getElementById("InputUserName").value=null;
+   document.getElementById("InputPassword").value=null;
+
+    }
 })
+
+
